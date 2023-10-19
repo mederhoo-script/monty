ac3357837dfda2671e76c415b1ae5e8aa25e7408
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-19 15:25:35 +0100

    monty wsap

--- swap.c
+++ swap.c
@@ -1,15 +1,25 @@
 #include "monty.h"
 
+/**
+ * swap - Swaps the top two elements of the stack.
+ *
+ * @stack: A pointer to the pointer to the stack.
+ * @line_number: The line number in the Monty file.
+ */
 void swap(stack_t **stack, unsigned int line_number)
 {
-	int temp;
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	stack_t *temp;
 
-    temp = (*stack)->n;
-    (*stack)->n = (*stack)->next->n;
-    (*stack)->next->n = temp;
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->next;
+	(*stack)->next = temp->next;
+	(*stack)->prev = temp;
+	temp->next = *stack;
+	temp->prev = NULL;
+	*stack = temp;
 }
