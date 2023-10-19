72d972d225e93636a87e3e07a337a72bf0da925d
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-19 14:00:33 +0100

    monty

--- main.c
+++ main.c
@@ -2,13 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * main - Monty interpreter main function.
- *
  * @argc: The number of arguments.
  * @argv: An array of strings containing the arguments.
- *
  * Return: 0 on success, EXIT_FAILURE on failure.
  */
 int main(int argc, char *argv[])
@@ -23,14 +20,12 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
 	while (fgets(line, sizeof(line), file) != NULL)
 	{
 		line_number++;
@@ -44,6 +39,13 @@ int main(int argc, char *argv[])
 			fprintf(stderr, "L%d: unknown instruction %s", line_number, line);
 			exit(EXIT_FAILURE);
 		}
+	}
+	while (stack != NULL)
+	{
+	stack_t *temp = stack;
+
+	stack = stack->next;
+	free(temp);
 	}
 
 	fclose(file);
